c8686147e7089f0dfb26a72dcd22f00be7bce740
Rafael França <rafaelfrancam@gmail.com>
2020-10-25 12:40:34 -0300

    Solution: List Transactions and Balance

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,4 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
 
 export default formatValue;
